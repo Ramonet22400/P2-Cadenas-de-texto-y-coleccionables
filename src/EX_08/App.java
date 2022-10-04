2db26f2d2e30b9e171011fbac7506196ab3080f7
Ramonet22400 <ramonperez22400@gmail.com>
2022-10-04 22:21:27 +0200

    EX_08 en proceso

--- src/EX_08/App.java
+++ src/EX_08/App.java
@@ -1,4 +1,17 @@
 package EX_08;
 
+// Escribe un programa que atraviese (itere) un ArrayList usando Lambdas.
+
+import java.util.ArrayList;
+
 public class App {
+
+    public static void main(String[] args){
+        EX_08.App app = new EX_08.App();
+        app.si();
+    }
+
+    public void si(){
+
+    }
 }
