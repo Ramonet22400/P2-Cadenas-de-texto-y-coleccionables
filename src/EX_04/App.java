8983f11e46c19662055bcd072df04be85b102ef7
Ramonet22400 <ramonperez22400@gmail.com>
2022-10-04 12:01:56 +0200

    EX_04 acabado

--- src/EX_04/App.java
+++ src/EX_04/App.java
@@ -1,4 +1,37 @@
 package EX_04;
+import java.util.Scanner;
+//Realice el ejercicio 3 usando indexOf.
 
 public class App {
+
+    Scanner v1 = new Scanner(System.in);
+    Scanner v2 = new Scanner(System.in);
+    public static void main(String[] args){
+        EX_04.App app = new EX_04.App();
+        app.Contador_index();
+    }
+
+    public void Contador_index(){
+
+        System.out.println("Escribe la palabra: ");
+        String palabra = v1.nextLine();
+        System.out.println("Introduzca el caracter: ");
+        String caracter = v2.nextLine();
+
+        String juntos = palabra + caracter;
+        String buscador ="/";
+
+        int indice = juntos.indexOf(buscador);
+
+        indice  = juntos.indexOf(buscador);
+
+        System.out.println("La poición del caracter es: " + indice);
+
+
+
+
+
+
+    }
+
 }
