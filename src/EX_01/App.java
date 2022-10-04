d1d546b24d52f6e4f91ab1dd7c6b93c748f34d2c
Ramonet22400 <ramonperez22400@gmail.com>
2022-10-04 10:19:07 +0200

    EX_01 acabado

--- src/EX_01/App.java
+++ src/EX_01/App.java
@@ -6,10 +6,26 @@ package EX_01;
 //“Francisco José López Pérez”, la salida será: “López Pérez, F. J.”.
 
 public class App {
+    String nombre_completo = "Ramon Perez Martin";
 
 
+    public static void main(String[] args){
+        App app = new App();
+        app.Apellidos();
+    }
 
+    public void Apellidos (){
 
+        String[] apellidos = nombre_completo.split(" ");
+        String apellido = apellidos[1];
+        String apellido2 = apellidos[2];
+        String nombre = apellidos[0];
 
+        char inicial = nombre.charAt(0);
+
+        System.out.println(apellido + " " + apellido2 + " " + inicial);
+
+    }
 
 }
+
