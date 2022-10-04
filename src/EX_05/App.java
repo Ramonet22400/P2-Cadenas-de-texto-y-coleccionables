fd574de156d6c009a5bba69f4bf72798dcbf6bfc
Ramonet22400 <ramonperez22400@gmail.com>
2022-10-04 20:26:28 +0200

    EX_05 acabado

--- src/EX_05/App.java
+++ src/EX_05/App.java
@@ -1,5 +1,6 @@
 package EX_05;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 //Dada una cadena de texto con una contraseña, indicar si la contraseña es fuerte o
 //débil. Consideramos una contraseña fuerte si contiene 8 o más caracteres, y entre
 //ellos, por lo menos una mayúscula, una minúscula, un símbolo y un dígito
@@ -35,7 +36,30 @@ public class App {
             System.out.println("La contraseña no contiene minsuculas y mayusculas.");
         }
 
+        Pattern pattern = Pattern.compile("^(?=.*\\d)(?=.*\\W)", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher("RAmon123:");
+        boolean contiene_digitos_simbolos = matcher.find();
 
-        if(contra1)
+        if(contiene_digitos_simbolos) {
+            System.out.println("La contraseña contiene tanto digítos como símbolos.");
+        } else {
+            System.out.println("La contaraseña no cumple con los parametros de seguridad");
+
+        }
+
     }
 }
+
+//Hay una forma mucho más sencilla de ejecutar este código, pero ya que este lo he pensado y creado yo desde 0 es el que eh puesto.
+
+
+//    Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*\\W).{8,}$", Pattern.CASE_INSENSITIVE);
+//    Matcher matcher = pattern.matcher("RAmon123:");
+//    boolean cumple_parametros = matcher.find();
+//    if(cumple_parametros) {
+//      System.out.println("La contraseña es segura :) ");
+//    } else {
+//      System.out.println("La contraseña no es segura :( ");
+//    }
+//  }
+
