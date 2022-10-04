f424e22bcdddfa9e46209eea62cba30849ef155f
Ramonet22400 <ramonperez22400@gmail.com>
2022-10-04 22:18:51 +0200

    EX_07 acabado

--- src/EX_07/App.java
+++ src/EX_07/App.java
@@ -3,27 +3,29 @@ package EX_07;
 //Escribe un programa que atraviese (itere) un ArrayList usando Iterator.
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class App {
 
     public static void main(String[] args){
-        EX_07.App app = new EX_07.App();
-        app.itere();
+        App app = new App();
+        app.iteracion();
     }
 
-    public void itere(){
+    public void iteracion(){
 
-        System.out.println();
         ArrayList<String> escuderias_F1 = new ArrayList<String>();
         escuderias_F1.add("Ferrari");
         escuderias_F1.add("Red Bull Racing");
         escuderias_F1.add("Mercedes AMG");
         escuderias_F1.add("Mclaren");
         escuderias_F1.add("Alpine F1 Team");
-        System.out.println(escuderias_F1);
 
+        Iterator<String> it = escuderias_F1.iterator();
 
+        for (int i = 0; i < escuderias_F1.size(); i++) {
 
-
+            System.out.println(it.next());
+        }
     }
 }
