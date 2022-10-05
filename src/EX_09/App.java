a169ccb57376edf05df71c3215d2e28a38bcba44
Ramonet22400 <ramonperez22400@gmail.com>
2022-10-05 09:49:45 +0200

    EX_09 acabado

--- src/EX_09/App.java
+++ src/EX_09/App.java
@@ -1,5 +1,9 @@
 package EX_09;
 
+//Escribe un programa que añada elementos a un hashSet y muestra el contenido.
+
+import java.util.HashSet;
+
 public class App {
 
     public static void main(String[] args){
@@ -9,6 +13,17 @@ public class App {
 
 
     public void hash_set(){
+
+        HashSet<Integer> haaaash = new HashSet<>();
+
+        haaaash.add(5);
+        haaaash.add(10);
+        haaaash.add(15);
+        haaaash.add(6);
+        haaaash.add(8);
+
+
+        System.out.println(haaaash);
 
     }
 }
